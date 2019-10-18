7be7e9d5fc172612776928fb749920ada01a2f96
mapogolions <ivanovpavelalex45@gmail.com>
2019-10-18 17:56:17 +0500

    Implement HttpRequest class

---
+++ Gof/Creational/Builder/Hierarchical/HttpRequest.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+namespace Gof.Creational.Builder.Hierarchical
+{
+    public class HttpRequest : HttpMessage
+    {
+        // required
+        private readonly string _method;
+        private readonly string _uri;
+        // optional
+        private readonly string _queryString;
+
+        private HttpRequest(InternalBuilder builder) : base(builder)
+        {
+            _method = builder.Method;
+            _uri = builder.Uri;
+            _queryString = builder.QueryString;
+        }
+
+        public override string ToString()
+        {
+            var statusLine = $"{_method} {_uri} HTTP/{_protocolVersion}".TrimEnd();
+            return $"{statusLine}\n{base.ToString()}";
+        }
+
+        public class InternalBuilder : MessageBuilder
+        {
+            // required
+            protected internal string Method { get; private set; }
+            protected internal string Uri { get; private set; }
+            // optional
+            protected internal string QueryString { get; private set; }
+
+            public InternalBuilder(string method, string uri) : this(method, uri, "1.1") {}
+
+            public InternalBuilder(string method, string uri, string protocolVersion) : base(protocolVersion)
+            {
+                Method = method;
+                Uri = uri;
+                QueryString = default;
+            }
+
+            public InternalBuilder WithQueryString(string queryString)
+            {
+                QueryString = queryString;
+                return this;
+            }
+
+            public override HttpMessage Build() => new HttpRequest(this);
+        }
+    }
+}
