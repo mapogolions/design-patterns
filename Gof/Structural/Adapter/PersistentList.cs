f3cd5034fce4c2164928621f04ec6f4f2e902b32
mapogolions <ivanovpavelalex45@gmail.com>
2019-12-07 15:04:59 +0500

    Little bit tweak

--- Gof/Structural/Adapter/PersistentList.cs
+++ Gof/Structural/Adapter/PersistentList.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Gof.Structural.Adapter
 {
     public static class PersistentList
@@ -13,38 +11,6 @@ namespace Gof.Structural.Adapter
                 return iter(new Cons<T>(items[index], tail), ++index);
             }
             return iter(null, 0);
-        }
-
-        public class Cons<T> : Iterable<T>
-        {
-            public T Head { get; }
-            public Cons<T> Tail { get; }
-
-            public Cons(T head, Cons<T> tail)
-            {
-                Head = head;
-                Tail = tail;
-            }
-
-            public Iterator<T> Iterator() => new ConsIterator<T>(this);
-        }
-
-        public class ConsIterator<T> : Iterator<T>
-        {
-            private  Cons<T> _origin;
-
-            public ConsIterator(Cons<T> origin) => _origin = origin;
-
-            public bool HasNext() => _origin is null;
-
-            public T Next()
-            {
-                if (_origin is null)
-                    throw new IndexOutOfRangeException();
-                var item = _origin.Head;
-                _origin = _origin.Tail;
-                return item;
-            }
         }
     }
 }
