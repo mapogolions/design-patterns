e29caf53dc7d8eda7c09f8bd40b2126b882ec5a8
mapogolions <ivanovpavelalex45@gmail.com>
2021-04-07 22:40:19 +0500

    implementation of eager singleton

---
+++ Gof/Creational/Singleton/EagerSingleton.cs
@@ -0,0 +1,11 @@
+﻿namespace Gof.Creational.Singleton
+{
+    public class EagerSingleton
+    {
+        private static readonly EagerSingleton _instance;
+
+        private EagerSingleton() { }
+
+        public static EagerSingleton Singleton => _instance;
+    }
+}
