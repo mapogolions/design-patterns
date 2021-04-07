417359413dc581878514ee1ffd6ee4efb27cbd50
mapogolions <ivanovpavelalex45@gmail.com>
2021-04-07 22:40:55 +0500

    implementation of the lazy singleton

---
+++ Gof/Creational/Singleton/LazySingleton.cs
@@ -0,0 +1,27 @@
+﻿namespace Gof.Creational.Singleton
+{
+    public class LazySingleton
+    {
+        private static LazySingleton _instance;
+
+        private LazySingleton() { }
+
+        public static LazySingleton Singleton
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    lock(_instance)
+                    {
+                        if (_instance is null)
+                        {
+                            _instance = new LazySingleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
