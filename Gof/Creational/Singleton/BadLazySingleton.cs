6028a2a454bcb1cbedcd713a770f0079d2e30825
mapogolions <ivanovpavelalex45@gmail.com>
2021-04-07 22:40:42 +0500

    implementation of the bad lazy singleton

---
+++ Gof/Creational/Singleton/BadLazySingleton.cs
@@ -0,0 +1,24 @@
+﻿namespace Gof.Creational.Singleton
+{
+    public class BadLazySingleton
+    {
+        private static BadLazySingleton _instance;
+
+        private BadLazySingleton() { }
+
+        public static BadLazySingleton Singleton
+        {
+            get
+            {
+                lock(_instance)
+                {
+                    if (_instance is null)
+                    {
+                        _instance = new BadLazySingleton();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
