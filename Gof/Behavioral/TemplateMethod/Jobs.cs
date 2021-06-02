97cb68e5d7d425164ef9a2ab91bd1b3574df2cb4
mapogolions <ivanovpavelalex45@gmail.com>
2021-06-03 02:31:06 +0500

    add some jobs

---
+++ Gof/Behavioral/TemplateMethod/Jobs.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gof.Behavioral.TemplateMethod
+{
+    public class TakeTimeJob : BackgroundJob
+    {
+        private readonly TimeSpan _takeTime;
+
+        public TakeTimeJob(TimeSpan takeTime) => _takeTime = takeTime;
+
+        protected override async Task ExecuteAsync(CancellationToken token = default)
+        {
+            await Task.Delay(_takeTime, token);
+        }
+    }
+
+    public class EndlessWaitingJob : TakeTimeJob
+    {
+        public EndlessWaitingJob() : base(Timeout.InfiniteTimeSpan) { }
+    }
+
+    public class CompletedAtStartTimeJob : BackgroundJob
+    {
+        protected override Task ExecuteAsync(CancellationToken token = default)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class NonCancellableJob : TakeTimeJob
+    {
+        public NonCancellableJob(TimeSpan takeTime) : base(takeTime) { }
+
+        protected override Task ExecuteAsync(CancellationToken token = default)
+        {
+            return base.ExecuteAsync(default);
+        }
+    }
+}
