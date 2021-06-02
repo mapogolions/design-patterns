f1efd4bf71cb1380afbf316e99dd56f332b5ca6b
mapogolions <ivanovpavelalex45@gmail.com>
2021-06-03 02:57:07 +0500

    update tests of template method pattern

--- Gof.Tests/Behavioral/TemplateMethodTests.cs
+++ Gof.Tests/Behavioral/TemplateMethodTests.cs
@@ -1,22 +1,73 @@
-using Gof.Creational.TemplateMethod;
+using System.Threading.Tasks;
 using Xunit;
+using Gof.Behavioral.TemplateMethod;
+using System;
 
 namespace Gof.Tests
 {
     public class TemplateMethodTests
     {
         [Fact]
-        public void ShouldReturnLinkToBuiltLostProfitReport()
+        public void StartAsyncRetunResult()
         {
-            var fake = new FakeClient(new LostProfitReport());
-            Assert.Equal("/Reports/LostProfitReport.xlsx", fake.BuildReport());
+            var backgroundJob = new CompletedAtStartTimeJob();
+            var executingTask = backgroundJob.RunAsync(new());
+
+            Assert.True(ReferenceEquals(executingTask, backgroundJob.ExecutingTask));
+            Assert.True(backgroundJob.ExecutingTask.IsCompletedSuccessfully);
+            Assert.True(executingTask.IsCompletedSuccessfully);
         }
 
         [Fact]
-        public void ShouldReturnLinkToBuiltPotentialRealProfitReport()
+        public async Task KillAsyncWaitsUntilBackgroundTaskHasBeenCompleted()
         {
-            var fake = new FakeClient(new PotentialRealProfitReport());
-            Assert.Equal("/Reports/PotentialRealProfitReport.xlsx", fake.BuildReport());
+            var backgroundJob = new NonCancellableJob(TimeSpan.FromMilliseconds(100));
+            await backgroundJob.RunAsync(new());
+
+            await backgroundJob.KillAsync(new());
+
+            Assert.True(backgroundJob.ExecutingTask.IsCompletedSuccessfully);
+        }
+
+        [Fact]
+        public async Task DisposeDoesNotGuaranteeCancellationOfTheBackgroundTask()
+        {
+            var backgroundJob = new NonCancellableJob(TimeSpan.FromMilliseconds(10));
+            await backgroundJob.RunAsync(new());
+
+            backgroundJob.Dispose();
+            Assert.False(backgroundJob.ExecutingTask.IsCanceled);
+
+            await Task.WhenAll(backgroundJob.ExecutingTask);
+            Assert.True(backgroundJob.ExecutingTask.IsCompletedSuccessfully);
+        }
+
+        [Fact]
+        public async Task KillAsyncShouldStopBackgroundTask()
+        {
+            var backgroundJob = new EndlessWaitingJob();
+            await backgroundJob.RunAsync(new());
+
+            Assert.False(backgroundJob.ExecutingTask.IsCompleted);
+
+            await backgroundJob.KillAsync(default);
+
+            Assert.True(backgroundJob.ExecutingTask.IsCompleted);
+            Assert.True(backgroundJob.ExecutingTask.IsCanceled);
+        }
+
+        [Fact]
+        public async Task DisposeShouldStopBackgroundTask()
+        {
+            var backgroundJob = new EndlessWaitingJob();
+            await backgroundJob.RunAsync(new());
+
+            Assert.False(backgroundJob.ExecutingTask.IsCompleted);
+
+            backgroundJob.Dispose();
+
+            Assert.True(backgroundJob.ExecutingTask.IsCompleted);
+            Assert.True(backgroundJob.ExecutingTask.IsCanceled);
         }
     }
 }
