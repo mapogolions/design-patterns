e05e51e12b9c292fdf4e4b6e2a6e885364ddede6
mapogolions <ivanovpavelalex45@gmail.com>
2021-06-03 02:56:44 +0500

    clean up template method

--- Gof/Behavioral/TemplateMethod/FakeClient.cs
+++
@@ -1,9 +0,0 @@
-namespace Gof.Creational.TemplateMethod
-{
-    public class FakeClient
-    {
-        private readonly Report _report;
-        public FakeClient(Report report) => _report = report;
-        public string BuildReport() => _report.BuildReport();
-    }
-}
